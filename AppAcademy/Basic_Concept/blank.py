2cab42db025ae66dc52f7169c6e4619cb6d23be2
Alan Tsai <alanalan216@gmail.com>
2023-08-27 12:00:07 -0700

    add 2nd python file

--- AppAcademy/Basic_Concept/blank.py
+++ AppAcademy/Basic_Concept/blank.py
@@ -3,6 +3,21 @@ def factorial(num):
         return 1
     else:
         return num * factorial(num-1)
-    
+
 result = factorial(4)
 print(result)
+
+
+def validate(num):
+    if (num < 1 or num < 10):
+        print("Out of range")
+    elif (num != int(num)):
+        print("Not an integer")
+    else:
+        print("You are right")
+        return True
+    return False
+
+print(validate(-5))
+print(validate(2.5))
+print(validate("sijjgi"))
---
+++ AppAcademy/Basic_Concept/sample.py
@@ -0,0 +1,18 @@
+def average(num1, num2):
+    return (num1/num2)
+
+print(average(6,2))
+
+print("Spaghetti".index("h"))
+print("Spaghetti".index("t"))
+print("Spaghetti".count("t"))
+print('''We choose to go to the moon in this decade and do the other things,
+not because they are easy, but because they are hard, because that goal will
+serve to organize and measure the best of our energies and skills, because that
+challenge is one that we are willing to accept, one we are unwilling to
+postpone, and one which we intend to win, and the others, too.
+'''.count('the '))
+
+first_name = "Billy"
+last_name = "Bob"
+print(f'Your name is {first_name} {last_name}')
