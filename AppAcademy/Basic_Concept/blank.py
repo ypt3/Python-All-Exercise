fad6daa740c924ff9c90aee9c67c1e94dcc68aaf
Alan Tsai <alanalan216@gmail.com>
2023-09-09 11:20:02 -0700

    more practice

--- AppAcademy/Basic_Concept/blank.py
+++ AppAcademy/Basic_Concept/blank.py
@@ -56,3 +56,46 @@ for i in range(len(nums) -3, -1, -1):
         print(nums[i] + nums[i + 1] > nums[i + 2])
     else:
         print(None)
+
+
+def seq_of_numbers(term):
+    term += ' '
+    i = 0
+    current_count = 1
+    res = ""
+    while i < len(term)-1:
+        if term[i] != term[i+1]:
+            res = res + str(current_count) + term[i]
+            current_count = 1
+        else:
+            current_count += 1
+        i += 1
+    return res
+
+print(seq_of_numbers("1211"))
+print(seq_of_numbers("111221"))
+
+def cap_space(str):
+    result = ""
+    i = 0
+    while i < len(str):
+        if str[i].isupper():
+            result += " "
+        result += str[i]
+        i += 1
+
+    return result.lower()
+
+print(cap_space("helloWorld"))
+
+def char_count(a, b):
+    count = 0
+    i = 0
+    while i < len(b):
+        if a == b[i]:
+            count += 1
+        i += 1
+    return count
+
+print(char_count("a", "App Academy"))
+
