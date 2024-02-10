d8d7ad6773a96957a4990e4c75a8f2191b7b3b36
Alan Tsai <alanalan216@gmail.com>
2024-02-10 10:17:44 -0800

    phase four task

--- AppAcademy/knight_travail/tests/path_finder.py
+++ AppAcademy/knight_travail/tests/path_finder.py
@@ -36,7 +36,31 @@ class KnightPathFinder:
                 current_node.add_child(child_node)
                 queue.append(child_node) # Enqueue the child node for further exploration
 
+    def find_path(self, end_position):
+        # Use breadth_search on the root node
+        end_node = self._root.breadth_search(end_position) 
+        if end_node is None:
+            return None # If end_position is not found, return None
+        return self.trace_to_root(end_node)
+    
+    def trace_to_root(self, end_node):
+        path = []
+        current_node = end_node
+
+        while current_node is not None:
+            # Insert at the beginning to maintain the correct order
+            path.insert(0, current_node.value)
+            current_node = current_node.parent
+        
+        return path
+
+
 finder = KnightPathFinder((0,0))
 # print(finder.new_move_positions((0,0)))
 finder.build_move_tree()
 print(finder._root.children)
+finder.build_move_tree()
+print(finder.find_path((2, 1)))  # Expected outcome: [(0, 0), (2, 1)]
+print(finder.find_path((3, 3)))  # Expected outcome: [(0, 0), (2, 1), (3, 3)]
+print(finder.find_path((6, 2)))  # Expected outcome: [(0, 0), (1, 2), (2, 4), (4, 3), (6, 2)]
+print(finder.find_path((7, 6)))  # Expected outcome: [(0, 0), (1, 2), (2, 4), (4, 3), (5, 5), (7, 6)]
